a1b322ec1db63d5f4c56079f8d8cbce11b9fe6a0
kornumic <kornumic@gmail.com>
2023-10-06 01:19:03 +0200

    Optimized and cleared code, fixed page 6 crash bug

--- backend/src/webScrapper/webScrapper.ts
+++ backend/src/webScrapper/webScrapper.ts
@@ -1,18 +1,18 @@
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
 
 let browser: Browser | null;
 
 export type Item = {
-  title: string | undefined;
-  address: string | undefined;
-  price: string | undefined;
-  src: string | undefined;
+  title: string;
+  address: string;
+  price: string;
+  src: string;
 };
 
 export const getBrowser = async () => {
   if (!browser) {
     browser = await puppeteer.launch({
-      headless: true,
+      headless: "new",
       defaultViewport: null,
     });
   }
@@ -26,72 +26,65 @@ export const closeBrowser = async () => {
   }
 };
 
+/**
+ * Scrapes data from loaded page. Waits for selector to appear.
+ * @param page
+ */
+const scrapeData = async (page: Page) => {
+  await page.waitForSelector("div.property.ng-scope a img", {
+    timeout: 10000,
+  });
+
+  return await page.evaluate(() => {
+    const listItems = Array.from(
+      document.querySelectorAll("div.property.ng-scope"),
+    );
+
+    const sources: Item[] = listItems.map((listItem): Item => {
+      const img = listItem.querySelector("a img");
+      const title = listItem.querySelector("h2");
+      const address = listItem.querySelector(".locality.ng-binding");
+      const price = listItem.querySelector(".norm-price.ng-binding");
+
+      return {
+        title: title?.innerText.toString() || "",
+        address: address?.textContent?.toString() || "",
+        price: price?.textContent?.toString() || "",
+        src: img?.getAttribute("src")?.toString() || "",
+      };
+    });
+    return sources;
+  });
+};
+
 export const webScrapper = async (
   baseUrl: string = "https://www.sreality.cz/en/search/for-sale/apartments?page=",
   quantity: number = 500,
   pageSize: number = 20,
 ) => {
-  // const parseElement = (element: Element) => {
-  //   const text = element.querySelector("h2")?.innerText;
-  //
-  //   return { text, author: "Sreality" };
-  // };
+  console.log("Initializing web scrapper...");
 
   const browser = await getBrowser();
-
   const page = await browser.newPage();
   const maxPage = quantity / pageSize;
-
   const all: Item[] = [];
+
+  console.log("Scraping data from sreality.cz ...");
   try {
     for (let currentPage = 1; currentPage <= maxPage; currentPage++) {
       const url = baseUrl + currentPage;
-      console.log(url);
-      await page.goto(url + currentPage, {
+      await page.goto(url, {
         waitUntil: "domcontentloaded",
       });
-      console.log("Page loaded");
-
-      await page.waitForSelector("div.property.ng-scope a img", {
-        timeout: 10000,
-      });
-
-      // Get page data
-      const data = await page.evaluate(() => {
-        const listItems = Array.from(
-          document.querySelectorAll("div.property.ng-scope"),
-        );
-
-        const sources: Item[] = listItems.map((listItem) => {
-          const img = Array.from(listItem.querySelectorAll("a img"));
-          const title = Array.from(listItem.querySelectorAll("h2"));
-          const address = Array.from(
-            listItem.querySelectorAll(".locality.ng-binding"),
-          );
-          const price = Array.from(
-            listItem.querySelectorAll(".norm-price.ng-binding"),
-          );
-
-          const item: Item = {
-            title: title[0].innerText.toString(),
-            address: address[0].textContent?.toString(),
-            price: price[0].textContent?.toString(),
-            src: img[0].getAttribute("src")?.toString(),
-          };
-          return item;
-        });
-        return [sources];
-      });
-
-      all.push(...data[0]);
+      const data = await scrapeData(page);
+      all.push(...data);
     }
-    console.log(all);
-    await closeBrowser();
-    console.log("Scrapping is done");
+    // console.log(all);
+    console.log("Successfully scraped " + all.length + " items.");
   } catch (err) {
-    console.log(err);
-    await browser.close();
-    console.log(all);
-    console.log(all.length);
+    console.log("Could not scrape data from sreality.cz");
+  } finally {
+    await closeBrowser();
   }
+  return all;
 };
