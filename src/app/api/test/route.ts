8ebccb04a3d744749ea0c839f5d3fed05ad6ea43
kornumic <kornumic@gmail.com>
2023-10-11 03:27:31 +0200

    Deleted unused test route

--- src/app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/lib/db";
-
-export const GET = async (request: NextRequest, res: NextResponse) => {
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
-};
